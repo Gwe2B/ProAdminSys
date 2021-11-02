01c1ec0e9142662ac3c2490c90ba871c67faf466
Gwe2B <gwenael2b@live.fr>
2021-11-02 18:23:57 +0100

    Add of better management of the subcommands
    Each subcommands are now in separate shell script to simplify code
    maintenance.

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# echo "=== Bottle Dowload & Installation  ==="
+# echo "=> Creation of an virtual environment & connect to it"
+# ~/.local/share/Python-3.10/bin/python3 -m venv .
+# . bin/activate
+# pip install -U bottle
+# echo "=> Disconnection of the virtual environment"
+# deactivate
--- script.sh
+++ script.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-pythonDlUrl="https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz"
-#OpenSSL is required, if is not installed pip will not function correctly
-opensslDlUrl="https://www.openssl.org/source/openssl-1.1.1g.tar.gz"
-
+scriptFolder="$(dirname $0)"
 outputFolder="output/"
 
 getHelp() {
@@ -22,55 +19,51 @@ HELP
 	return 1
 }
 
-while getopts ':ho:' option; do
-	case $option in
-		h)
+main() {
+	while getopts ':ho:' option; do
+		case $option in
+			h)
+				getHelp
+				;;
+			
+			o)
+				outputFolder=${OPTARG}
+				;;
+
+			\?)
+				echo "Invalid option -$option \n" >&2
+				getHelp >&2
+				exit 1
+				;;
+		esac
+	done
+	shift $((OPTIND-1))
+
+	# Remove main command from the args list
+	subcommand="${1:-}"
+	if [ -z ${subcommand} ]; then
+		echo "Please enter a subcommand!"
+		getHelp
+		return 0
+	fi
+
+	shift
+	case "${subcommand}" in
+		install)
+			${scriptFolder}/install.sh
+			exit 0
+			;;
+
+		build)
+			${scriptFolder}/build.sh
+			exit 0
+			;;
+
+		*)
+			echo "Invalid subcommand: ${subcommand}!"
 			getHelp
 			;;
-		
-		o)
-			outputFolder=${OPTARG}
-			;;
-
-		\?)
-			echo "Invalid option -$option \n" >&2
-			getHelp >&2
-			exit 1
-			;;
 	esac
-done
-shift $((OPTIND-1))
+}
 
-# if [ $1 = "install" ]
-# then
-# 	mkdir $outputFolder "${outputFolder}/downloads"
-# 	cd "${outputFolder}/downloads"
-
-# 	echo "=== OpenSSL Download & Installation ==="
-# 	wget $opensslDlUrl -O openssl.tar.gz
-# 	tar -xzf openssl.tar.gz
-# 	cd openssl-1.1.1g
-# 	./config --prefix="$HOME/openssl" --openssldir="$HOME/openssl" no-ssl2
-# 	make
-# 	make install
-# 	cd ..
-
-# 	echo "=== Python Download & Installation ==="
-# 	wget $pythonDlUrl -O python310.tgz
-# 	tar -xzf python310.tgz
-# 	cd "Python-3.10.0"
-# 	./configure --prefix="$HOME/.local/share/Python-3.10" --enable-optimizations --with-openssl="$HOME/openssl/"
-# 	make install -j3
-
-# 	echo "=== Cleaning ==="
-# 	cd ../..
-# 	rm -r downloads
-
-# 	echo "=== Bottle Dowload & Installation  ==="
-# 	echo "=> Creation of an virtual environment & connect to it"
-# 	~/.local/share/Python-3.10/bin/python3 -m venv .
-# 	. bin/activate
-# 	pip install -U bottle
-# 	echo "=> Disconnection of the virtual environment"
-# 	deactivate
-# fi
+main ${@}
