d23644f7dcd947483b10bc78395af0764f4dfbcb
marioncalpe <marioncalpe2a@gmail.com>
2021-12-06 19:11:21 +0100

    Ajout de la fonction d'affichage des images
    Ajout d'une fonction de récuperation du chemin d'acces des images et affichage.

--- site/main.py
+++ site/main.py
@@ -1,4 +1,8 @@
 from bottle import *
+from os import listdir
+from os.path import isfile, join 
+
+IMG_FOLDER = './static/img'
 
 app = Bottle()
 
@@ -8,6 +12,10 @@ def serv_static(filename):
 
 @app.route('/')
 def index():
-    return template('views/index.tlp')
+    lst_img = [join(IMG_FOLDER,f) for f in listdir(IMG_FOLDER) if isfile(join(IMG_FOLDER,f))]
+    my_dict = dict([('images', lst_img)])
+    print(lst_img)
+    return template('views/index.tlp', my_dict)
+
 
 run(app, host='localhost', debug=True, reloader=True, port=8080)
